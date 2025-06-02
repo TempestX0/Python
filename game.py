c8f94d363a7a778bf616c10f9c8a4d1ece239666
TempestX0 <brianskidmore@gmail.com>
2025-06-02 13:50:57 -0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,37 @@
+import random
+
+
+def guessing_game():                            # Create a function named guessing_game
+    # Generate a random number between 1 and 100
+    target_number = random.randint(1, 100)
+    guess = 0
+    number_guess = 1
+
+    print(" ")
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number from 1 to 100.")
+
+    while guess != target_number:
+        try:
+            guess = int(input("Enter your guess: "))
+            if guess < target_number:
+                print("Too low! Try again.")
+                number_guess += 1
+            elif guess > target_number:
+                print("Too high! Try again.")
+                number_guess += 1
+            else:
+                print(" ")
+                print(
+                    f"Congratulations! You guessed the number {target_number} correctly!")
+                print(
+                    f"It took you {number_guess} guesses.")
+                print(" ")
+
+        except ValueError:
+            print("¯\_(ツ)_/¯")
+            print("Please enter a valid number.")
+
+
+if __name__ == "__main__":
+    guessing_game()
